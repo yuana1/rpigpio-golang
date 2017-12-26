539a4d400b6ed4d2e3f4fb1926b61d43a7f0ea89
zhaokaiyuan <zhaoky0918@gmail.com>
2017-12-26 17:41:34 +0800

    init

---
+++ wiringPi/include/print.h
@@ -0,0 +1,17 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#ifdef __cplusplus
+extern " C " {
+#endif
+
+//打印点东西
+void Print(int iNum);
+
+void PrintStr(char* str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
